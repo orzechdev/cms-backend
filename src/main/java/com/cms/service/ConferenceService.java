284ec17fec6f8b1e1bb8cd5a82980b224970930f
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-01-03 02:29:38 +0100

    Conference Service created

---
+++ src/main/java/com/cms/service/ConferenceService.java
@@ -0,0 +1,40 @@
+package com.cms.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cms.entity.Conference;
+import com.cms.repository.ConferenceRepository;
+
+@Service
+public class ConferenceService {
+	
+	@Autowired
+	private ConferenceRepository conferenceRepository;
+	
+	public List<Conference> getAllConferences() {
+		List<Conference> conferences = new ArrayList<>();
+		conferenceRepository.findAll().forEach(conferences::add);
+		return conferences;
+	}
+
+	public Conference getConference(Integer conferenceId) {
+		return conferenceRepository.findById(conferenceId).get();
+	}
+
+	public void addConference(Conference conference) {
+		conferenceRepository.save(conference);
+	}
+	
+	public void updateConference(Conference conference) {
+		conferenceRepository.save(conference);
+	}
+	
+	public void deleteConference(Integer conferenceId) {
+		conferenceRepository.deleteById(conferenceId);		
+	}
+
+}
