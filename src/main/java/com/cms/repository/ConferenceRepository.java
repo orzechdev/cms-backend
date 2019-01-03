514c8fd9912e639cd66f9ee7335d7b7f6179ae28
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-01-03 02:34:06 +0100

    Conference Repository added

---
+++ src/main/java/com/cms/repository/ConferenceRepository.java
@@ -0,0 +1,8 @@
+package com.cms.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.cms.entity.Conference;
+
+public interface ConferenceRepository extends CrudRepository<Conference, Integer>{
+
+}
