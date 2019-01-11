db4486c05817b66be8656919a5ca9f91ddf81aa5
RM <r.mlynczyk@gmail.com>
2019-01-11 16:10:23 +0100

    Greetings test endpoint seems to have outlived it's usefulness

--- src/main/java/com/cms/repository/GreetingRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.cms.repository;
-//
-//import org.springframework.data.repository.CrudRepository;
-//import com.cms.entity.Greeting;
-//
-//
-//public interface GreetingRepository extends CrudRepository<Greeting,Integer> {
-//}
