84a93b4c0d8a6474c623764d247662a71be6b451
orzechdev <tarasiukdawid@gmail.com>
2019-01-13 01:37:03 +0100

    File upload / download from Purvam

--- src/main/java/com/cms/Application.java
+++ src/main/java/com/cms/Application.java
@@ -1,13 +1,18 @@
 package com.cms;
 
+import com.cms.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.cms")
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class Application extends SpringBootServletInitializer {
 
     @Override
