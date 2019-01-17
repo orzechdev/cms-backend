a10b1ad138e80ade42c4d481b42d851910103ffa
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-01-17 23:49:36 +0100

    Attendance Repository added.

--- src/main/java/com/cms/repository/AttendanceRepository.java
+++ src/main/java/com/cms/repository/AttendanceRepository.java
@@ -1,11 +1,13 @@
 
 package com.cms.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
-
 import com.cms.entity.Attendance;
-
-
+import com.cms.entity.Conference;
 
 public interface AttendanceRepository extends CrudRepository<Attendance,Integer> {
+	public List<Attendance> findAllByRoleAndConferenceID(String role, Conference conferenceId);
+	public List<Attendance> findAllByConferenceID(Conference conferenceId);
 }
