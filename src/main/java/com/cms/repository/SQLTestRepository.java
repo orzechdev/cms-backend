ef9173d81b6730b94ddb3293454b61829830ed09
RM <r.mlynczyk@gmail.com>
2019-01-18 06:04:06 +0100

    Removed unused test endpoint

--- src/main/java/com/cms/repository/SQLTestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cms.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.cms.entity.SQLTest;
-
-
-public interface SQLTestRepository extends CrudRepository<SQLTest,Integer> {
-}
